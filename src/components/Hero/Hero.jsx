eb50320378ac3d64145b39e6684a6e2316edb932
g-AbrahamGarcia <hazel141272@gmail.com>
2024-07-02 12:09:29 -0600

    Cambios de hero.jsx

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
     <div className='Hero container' id='encabezado'>
       <div className="hero-text">
         <h1>Garantizamos viajes seguros en un mundo conectado </h1>
-        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Suscipit molestias maxime inventore voluptatum earum reprehenderit rem nisi quaerat saepe obcaecati, distinctio recusandae repudiandae hic beatae harum eaque. Aspernatur, quia sequi.</p>
+        <p>Ofrecemos transporte de químicos confiable y seguro, con estándares rigurosos de seguridad, puntualidad y integridad del producto. Nuestro equipo capacitado y tecnología avanzada garantizan eficiencia y tranquilidad para nuestros clientes.</p>
         <button className='btn'> Explorar mas <img src={dark_arrow} alt="" /></button>
       </div>
     </div>
